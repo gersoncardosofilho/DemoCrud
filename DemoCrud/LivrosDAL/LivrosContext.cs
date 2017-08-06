ed8305187a91c00fb780b073f28a6f318fa180ec
Gerson Cardoso Filho <gersoncfilho@mac.com>
2017-08-06 11:24:12 -0300

    First commit

---
+++ DemoCrud/LivrosDAL/LivrosContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using DemoCrud.Models;
+
+namespace DemoCrud.LivrosDAL
+{
+    public class LivrosContext : DbContext
+    {
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Properties<string>().Configure(c=>c.HasMaxLength(100));
+        }
+    }
+}
